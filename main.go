59193146e4e32f0b7647c1b7409764d9b4769ac9
hateqwq <critsuk47@gmail.com>
2025-05-02 10:15:52 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "github.com/hateqwq/vehicles"
+)
+
+func main() {
+    // Пример использования функционала из vehicles
+    vehicle := vehicles.NewCar("Tesla", "Model 3")
+    fmt.Println("Vehicle:", vehicle)
+}
+
